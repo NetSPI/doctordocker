6cc92a0eb7bf8f3a1ca6e89ab8a8cc1ca42f9c19
Patrick Cooley <patrick.cooley@nvisium.com>
2014-10-13 21:15:08 -0400

    removed gems from brain directory

--- brain/gems/ruby/2.1.0/gems/docker-api-1.13.6/spec/support/vcr.rb
+++
@@ -1,9 +0,0 @@
-require 'vcr'
-require 'webmock'
-
-VCR.configure do |c|
-  c.allow_http_connections_when_no_cassette = false
-  c.hook_into :excon, :webmock
-  c.cassette_library_dir = File.join(File.dirname(__FILE__), '..', 'vcr')
-  c.configure_rspec_metadata!
-end
